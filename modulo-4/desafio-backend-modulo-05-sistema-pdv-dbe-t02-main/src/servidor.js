3d768207bacc34ca8e1c12451156410278d06a9e
Felipe Jose Fernandes <124213684+FelipejFernandes@users.noreply.github.com>
2023-11-02 17:34:42 -0300

    Delete modulo-4/desafio-backend-modulo-05-sistema-pdv-dbe-t02-main directory

--- modulo-4/desafio-backend-modulo-05-sistema-pdv-dbe-t02-main/src/servidor.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-const servidor = app.listen(3000, () => {
-  console.log("Servidor iniciado..........");
-});
-
-module.exports = servidor;
